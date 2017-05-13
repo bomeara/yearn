51fe2363a10aff2af3e82c67f411de511d045b93
Brian O'Meara <omeara.brian@gmail.com>
2017-05-12 23:29:27 -0400

    updated to allow selection of packages

--- R/yearn.R
+++ R/yearn.R
@@ -1,7 +1,8 @@
 #' Do yearn on a single package
 #' @param pkg The string with the package name
+#' @param prompt Ask user which of several options to choose
 #' @return NULL if success, a string describing the problem ohterwise
-yearn.one <- function(pkg) {
+yearn.one <- function(pkg, prompt=FALSE) {
   failure.type <- NULL
   if(!suppressWarnings(require(pkg, character.only=TRUE, quietly=TRUE))) {
     suppressWarnings(utils::install.packages(pkg, quiet=TRUE, verbose=FALSE))
@@ -17,7 +18,15 @@ yearn.one <- function(pkg) {
         failure.type <- paste(pkg, ": no matches for", pkg)
       }
       if(length(potential.packages)>1) {
-        failure.type <- paste(pkg, ": there were", length(potential.packages),"matches for", pkg, "on github")
+        if(!prompt) {
+          failure.type <- paste(pkg, ": there were", length(potential.packages),"matches for", pkg, "on github")
+        } else {
+          cat(paste("Need user input for package", pkg,"\n\n"))
+          githubinstall::githubinstall(pkg)
+          if(!suppressWarnings(require(pkg, character.only=TRUE, quietly=TRUE))) {
+            failure.type <- paste(pkg, ": there were", length(potential.packages),"matches for", pkg, "on github; tried to install one, but it still would not load")
+          }
+        }
       }
     }
   }
@@ -27,10 +36,11 @@ yearn.one <- function(pkg) {
 #' Do yearn on a vector
 #' @param package A vector of packages (could be length 1)
 #' @param success.return If FALSE, only return info on failing packages
+#' @param prompt If TRUE, ask user to choose between options.
 #' @return A list of return strings (NULL if installed)
 #' @export
-yearn <- function(package, success.return=FALSE) {
-  result <- sapply(package, yearn.one)
+yearn <- function(package, success.return=FALSE, prompt=TRUE) {
+  result <- sapply(package, yearn.one, prompt=prompt)
   if(!success.return) {
     result <- result[!sapply(result, is.null)]
   }
