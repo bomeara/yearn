c5753d3169a4f2ac46965eecfd87bca8a565fc7f
Brian O'Meara <omeara.brian@gmail.com>
2017-06-10 14:33:08 -0700

    updates to fix version number and devtools::check()

--- R/yearn.R
+++ R/yearn.R
@@ -10,6 +10,10 @@
 #' @details
 #' See the readme file. Basically, this tries to load an installed package. If that fails, it then looks (in order) on Bioconductor, CRAN, GitHub's CRAN mirror, and other GitHub repositories for the package and installs it if it can find it.
 yearn <- function(pkg, maxdist=2, username.pref = c("cran", "ropensci", "rstudio", "tidyverse", "hadley", "yihui", "RcppCore", "eddelbuettel", "ropenscilabs", "hrbrmstr", "thej022214", "bomeara")) {
+  original.repos <- options("repos")
+  if(original.repos=='@CRAN@') {
+    options(repos = 'https://cloud.r-project.org/')
+  }
   pkg <- deparse(substitute(pkg))
   failure.type <- NULL
   if(!suppressWarnings(require(pkg, character.only=TRUE, quietly=TRUE))) {
@@ -21,13 +25,14 @@ yearn <- function(pkg, maxdist=2, username.pref = c("cran", "ropensci", "rstudio
       }
     } else {
       print(paste(pkg, "not installed, now going to try to find it on CRAN or Bioconductor"))
-      source("https://bioconductor.org/biocLite.R")
+    #  source("https://bioconductor.org/biocLite.R")
       on.repo <- utils::available.packages(repos=BiocInstaller::biocinstallRepos())[,"Package"]
       to.install <- on.repo[grepl(paste0('^', pkg, '$'),on.repo,ignore.case=TRUE)] #based on answer from https://stackoverflow.com/questions/39996324/case-insensitive-package-installation-ignore-case-while-installing-packages/39996637
       if(length(to.install)==1 && pkg != to.install) {
         print(paste("You asked for", pkg, "but it's actually", matching.installed, "on CRAN. Package names are case-sensitive. Will try to load anyway, but fix this in the future."))
         pkg <- to.install
       }
+
       suppressWarnings(utils::install.packages(pkg, quiet=FALSE, verbose=FALSE))
       if(!suppressWarnings(require(pkg, character.only=TRUE, quietly=TRUE))) {
         print(paste("Installing", pkg, "from CRAN failed, now trying to install from github. First trying from CRAN mirror (which includes packages formerly on CRAN). Note this is case-sensitive."))
@@ -55,6 +60,9 @@ yearn <- function(pkg, maxdist=2, username.pref = c("cran", "ropensci", "rstudio
         print(paste("Successfully installed", pkg, "from CRAN"))
       }
     }
+  }
+  if(original.repos=='@CRAN@') {
+    options(repos = original.repos) #make no permanent changes
   }
   return(invisible(failure.type))
 }
@@ -86,7 +94,7 @@ FindClosestPackage <- function(pkg, maxdist=2, auto.select=TRUE, username.pref =
   if(length(closest)>0 & auto.select) {
     matching.names <- which(username.pref %in% best.matches$username)
     if(length(matching.names)>=1) {
-      best.matches <- subset(best.matches, username == username.pref[matching.names[1]])[1,]
+      best.matches <- subset(best.matches, best.matches$username == username.pref[matching.names[1]])[1,]
     }
   }
   final.names <- paste(best.matches$username, best.matches$package_name, sep="/")
